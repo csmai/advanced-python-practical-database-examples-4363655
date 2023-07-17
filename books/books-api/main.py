842df034845448304972d1f7989f8109a2d4a309
csmai <maicsilla@gmail.com>
2023-07-17 16:16:32 +0200

    debug elkezdese - rogzites

--- books/books-api/main.py
+++ books/books-api/main.py
@@ -1,21 +1,45 @@
 from fastapi import FastAPI
-from . import schemas
-from . import database
+import schemas
+import database
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, filename="server_logs.log")
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+
 @app.get("/")
 def get_root():
-	return "Welcome to the books api"
+    return "Welcome to the books api"
+
 
 @app.post("/book/")
 def create_book(request: schemas.BookAuthorPayload):
-	database.add_book(convert_into_book_db_model(request.book), convert_into_author_db_model(request.author))
-	return "New book added " + request.book.title + " " + str(request.book.number_of_pages) \
-	+ " New author added " + request.author.first_name + " " + request.author.last_name
+    try:
+        database.add_book(
+            convert_into_book_db_model(request.book),
+            convert_into_author_db_model(request.author),
+        )
+        return (
+            "New book added "
+            + request.book.title
+            + " "
+            + str(request.book.number_of_pages)
+            + " New author added "
+            + request.author.first_name
+            + " "
+            + request.author.last_name
+        )
+    except Exception as e:  # Catch specific exception type if possible.
+        logger.error(f"An error occurred while adding a new book: {str(e)}")
+        raise  # Re-raise the exception to provide proper feedback to the client.
+
 
 def convert_into_book_db_model(book: schemas.Book):
-	return database.Book(title=book.title, number_of_pages=book.number_of_pages)
+    return database.Book(title=book.title, number_of_pages=book.number_of_pages)
+
 
 def convert_into_author_db_model(author: schemas.Author):
-	return database.Author(first_name=author.first_name, last_name=author.last_name)
+    return database.Author(first_name=author.first_name, last_name=author.last_name)
