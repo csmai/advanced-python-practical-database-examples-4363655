81ab9b1e2e1033a3f6fc981c6caf7a27b763d19f
csmai <maicsilla@gmail.com>
2023-07-17 16:35:39 +0200

    quick fix - sajat verziora update

--- books/books-api/main.py
+++ books/books-api/main.py
@@ -3,16 +3,14 @@ import schemas
 import database
 import logging
 
+app = FastAPI()
+
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
-    filename="server_logs.log",
+    level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="server_logs.log",
 )
-logger = logging.getLogger(__name__)
-
-app = FastAPI()
 
 
 @app.get("/")
@@ -23,28 +21,29 @@ def get_root():
 @app.post("/book/")
 def create_book(request: schemas.BookAuthorPayload):
     try:
-        database.add_book(
-            convert_into_book_db_model(request.book),
-            convert_into_author_db_model(request.author),
-        )
-        return (
-            "New book added "
-            + request.book.title
-            + " "
-            + str(request.book.number_of_pages)
-            + " New author added "
-            + request.author.first_name
-            + " "
-            + request.author.last_name
-        )
-    except Exception as e:  # Catch specific exception type if possible.
-        logger.error(f"An error occurred while adding a new book: {str(e)}")
-        raise  # Re-raise the exception to provide proper feedback to the client.
+        author = conv_payload_to_Author(request.author)
+        book = conv_payload_to_Book(request.book)
+
+        # Log the book and author details
+        logging.info("Adding Book: %s with %d pages.", book.title, book.number_of_pages)
+        logging.info("Adding Author: %s %s", author.first_name, author.last_name)
+
+        database.add_book(author, book)
+
+        response_msg = f"""New Book added: {book.title} with {str(book.number_of_pages)} pages. Author: {author.first_name} {author.last_name}"""
+        logging.info(response_msg)
+        return response_msg
+    except Exception as e:
+        # Log any exceptions that occur during book creation
+        logging.error("Error adding book: %s", str(e))
+        return "Failed to add book. Please check the server logs for details."
 
 
-def convert_into_book_db_model(book: schemas.Book):
-    return database.Book(title=book.title, number_of_pages=book.number_of_pages)
+def conv_payload_to_Book(book_pl: schemas.Book):
+    return database.Book(title=book_pl.title, number_of_pages=book_pl.number_of_pages)
 
 
-def convert_into_author_db_model(author: schemas.Author):
-    return database.Author(first_name=author.first_name, last_name=author.last_name)
+def conv_payload_to_Author(author_pl: schemas.Author):
+    return database.Author(
+        first_name=author_pl.first_name, last_name=author_pl.last_name
+    )
