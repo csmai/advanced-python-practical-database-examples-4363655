bdbc76cd115203028cd5a0efd28b4827c9e4c12e
csmai <maicsilla@gmail.com>
2023-07-28 15:02:46 +0200

    add project

--- project-tracker-workspace/app.py
+++ project-tracker-workspace/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from database import engine_conn_string
 
 app = Flask(__name__)
 
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = engine_conn_string()
 app.config[
     "SECRET_KEY"
@@ -20,26 +20,43 @@ class Project(db.Model):
     title = db.Column(db.String(length=50))
 
 
+class Task(db.Model):
+    __tablename__ = "tasks"
+
+    task_id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey(Project.project_id))
+    description = db.Column(db.String(length=50))
+
+    project = db.relationship("Project")
+
+
 print("This is a printed message.")
 
 
 # Define a route
 @app.route("/")
 def show_projects():
-    print("Project.query.all()")
-    app.logger.error(Project.query.all())
-    app.logger.error(Project.query.all()[0].project_id)
     return render_template("index.html", projects=Project.query.all())
 
 
 @app.route("/project/<project_id>")
 def show_tasks(project_id):
-    return render_template("project-tasks.html", project_id=project_id)
+    app.logger.error(Task.query.filter_by(project_id=project_id).all())
+    return render_template(
+        "project-tasks.html",
+        project=Project.query.filter_by(project_id=project_id).first(),
+        tasks=Task.query.filter_by(project_id=project_id).all(),
+    )
 
 
 @app.route("/add/project", methods=["POST"])
 def add_project():
-    # TODO: Add project
+    if request.form["project-title"] is not None:
+        new_project = Project(title=request.form["project-title"])
+        db.session.add(new_project)
+        db.session.commit()
+    else:
+        flash("Please enter a project title!", color="red")
     return "Project added successfully"
 
 
